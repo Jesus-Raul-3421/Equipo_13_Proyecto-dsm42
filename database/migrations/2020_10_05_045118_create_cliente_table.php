c5d719c6b5384c90d064d533fbe3ccbc96001452
Jesus Raul <al221910356@gmail.com>
2020-10-05 00:05:37 -0500

    Migraciones con llaves Foraneas

---
+++ database/migrations/2020_10_05_045118_create_cliente_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClienteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cliente', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->string('apellido');
+            $table->string('correo');
+            $table->integer('telefono');
+            $table->foreign('id')->references('id')->on('renta');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cliente');
+    }
+}
